e55a78cb057f5e1711f74f1db50b784469687dd4
HoudaSebti <sebti.houda14@gmail.com>
2020-10-21 19:32:22 +0200

    test fix 1

--- tests/test_preprocessor.py
+++ tests/test_preprocessor.py
@@ -7,17 +7,18 @@ from utilities import argument_parser
 
 
 class TestPreprocessorFuncs(unittest.TestCase):
-    def test_path_canity_check(self, image_path):
-        image_visualizer.path_sanity_check(image_path)
+    def test_path_sanity_check(self):
+        try:
+            image_visualizer.path_sanity_check(
+                os.path.join(
+                    args.data_path,
+                    'train/pneumonia/person993_bacteria_2000921.jpeg'
+                )
+            )
+        except ExceptionType:
+            self.fail("The given image path does not exist!")
 
-    def test_image_visualizer(self, image_path):
-        image_visualizer.visualize_image(image_path)
 
 if __name__ == '__main__':
     args = argument_parser.parse_args()
-    unittest.main(
-        os.path.join(
-            args.data_path,
-            'train/pneumonia/person993_bacteria_2921.jpeg'
-        )
-    )
+    unittest.main( )
