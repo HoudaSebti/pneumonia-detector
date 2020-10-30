96320c328051a15591fd21fc48bc0d7e56baef98
HoudaSebti <sebti.houda14@gmail.com>
2020-10-30 19:24:38 +0100

    visualizing the image

--- preprocessor/preprocessor/image_visualizer.py
+++ preprocessor/preprocessor/image_visualizer.py
@@ -1,7 +1,11 @@
 import os
 
 import numpy
+
+from utilities import argument_parser
+
 import matplotlib.pyplot as plt
+from PIL import Image
 
 def path_sanity_check(image_path):
     if not os.path.isfile(image_path):
@@ -11,3 +15,16 @@ def visualize_image(image_path):
     path_sanity_check(image_path)
     plt.imshow(image_path)
     plt.show()
+
+def get_image_shape(image_path):
+    image = Image.Open(image_path)
+    return image.size
+
+if __name__ == '__main__':
+    args = argument_parser.parse_args()
+    print(
+        get_image_shape(
+            args.data_path
+        )
+    )
+    visualize_image(args.data_path)
