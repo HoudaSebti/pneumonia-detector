e570de6c26eb918c99b3c7c704eb85f29def0781
HoudaSebti <sebti.houda14@gmail.com>
2020-11-14 11:09:30 +0100

    getting the dataset

--- preprocessor/preprocessor/data_set_generator.py
+++ preprocessor/preprocessor/data_set_generator.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 from enum import Enum
 
+import glob
+
 from utilities import argument_parser
 
 class Dataset_type(Enum):
@@ -10,7 +12,33 @@ class Dataset_type(Enum):
     TEST = 'test'
     VAL = 'val'
 
+def sanity_check(data_set_type):
+    if not isinstance(data_set_type, Dataset_type):
+        raise TypeError('the dataset type shoud be an instance od Fataset_type class !')
+
 def generate_dataset(data_set_type, data_path=None):
+    sanity_check(data_set_type)
     args = argument_parser.parse_args()
     if data_path is None: 
         data_path = args.data_path
+    images_paths = np.vstack(
+        [
+            glob.glob(
+                os.path.join(
+                    data_path,
+                    data_set_type.value,
+                    '%s/*.jpeg' %pathology
+                ) 
+            ) for pathology in ['normal', 'pneumonia']
+        ]
+    )
+    return pd.Dataframe(
+        data={
+            'image_path' : images_paths,
+            'label' : np.array(
+                [
+                    0 if 'normal' in image_path else 1 for image_path in images_paths
+                ]
+            )
+        }
+    )
