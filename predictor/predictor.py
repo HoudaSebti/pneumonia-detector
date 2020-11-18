e19ea4920f88ad755f76ba4c10e230581dbfed76
HoudaSebti <sebti.houda14@gmail.com>
2020-11-18 19:48:32 +0100

    minor fix

--- predictor/predictor.py
+++ predictor/predictor.py
@@ -1,5 +1,5 @@
 from predictor import model_generator
 
 if __name__ == '__main__':
-    model_generator.build_model((224,224,3))
+    model=model_generator.build_model((224,224,3))
     model.summary()
