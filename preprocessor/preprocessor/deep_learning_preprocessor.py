a49115e63cb62bc3c85acfda3fe3d58d2a3e1b1e
HoudaSebti <sebti.houda14@gmail.com>
2020-12-07 20:21:50 +0100

    bug fix

--- preprocessor/preprocessor/deep_learning_preprocessor.py
+++ preprocessor/preprocessor/deep_learning_preprocessor.py
@@ -7,18 +7,6 @@ import numpy as np
 def preprocess_batch(images_batch, labels_batch):
     transform = T.Compose([T.ToTensor()])
     return (
-        torch.tensor(
-            np.array(
-                [
-                    transform(image) for image in images_batch
-                ]
-            )
-        ),
-        torch.tensor(
-            np.array(
-                [
-                    label for label in labels_batch
-                ]
-            )
-        )       
+        torch.tensor(images_batch),
+        torch.tensor(labels_batch)       
     )
