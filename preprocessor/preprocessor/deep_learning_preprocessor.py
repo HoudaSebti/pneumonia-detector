4941f49a7922994e27d019ff236f8f7d6b925d82
HoudaSebti <sebti.houda14@gmail.com>
2020-12-07 19:25:09 +0100

    trying to fix the bug

--- preprocessor/preprocessor/deep_learning_preprocessor.py
+++ preprocessor/preprocessor/deep_learning_preprocessor.py
@@ -4,4 +4,4 @@ from torchvision import transforms as T
 
 def preprocess_batch(images_batch):
     transform = T.Compose([T.ToTensor()])
-    return [trasform(image) for image in images_batch]
+    return [transform(image) for image in images_batch]
