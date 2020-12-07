24168e0f87ba59586d1e649955ce41373cba0489
HoudaSebti <sebti.houda14@gmail.com>
2020-12-07 19:38:33 +0100

    convert images to pytorch tensors

--- preprocessor/preprocessor/deep_learning_preprocessor.py
+++ preprocessor/preprocessor/deep_learning_preprocessor.py
@@ -4,4 +4,10 @@ from torchvision import transforms as T
 
 def preprocess_batch(images_batch):
     transform = T.Compose([T.ToTensor()])
-    return [transform(image) for image in images_batch]
+    return torch.tensor(
+        np.array(
+            [
+                transform(image) for image in images_batch
+            ]
+        )
+    )
