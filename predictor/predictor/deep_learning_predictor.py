1b9030dc75b7c5a7f6c9f54080f3eacf79f62b60
HoudaSebti <sebti.houda14@gmail.com>
2020-12-11 19:36:18 +0100

    with balanced dataset

--- predictor/predictor/deep_learning_predictor.py
+++ predictor/predictor/deep_learning_predictor.py
@@ -1,10 +1,11 @@
 import sys
+import numpy as np
 
 import torch
 import torch.nn as nn
 from torchvision import models, transforms
+import torch.optim as optim
 from utilities import data_utils
-
 
 from keras.models import Sequential, Model
 from keras.applications.vgg16 import VGG16, preprocess_input
@@ -19,10 +20,10 @@ from keras.optimizers import Adam, SGD, RMSprop
 from keras.callbacks import ModelCheckpoint, Callback, EarlyStopping
 
 from preprocessor import deep_learning_preprocessor
-import torch.optim as optim
+import matplotlib.pyplot as plt
 
 
-def train_on_batch(batch_images, batch_labels, model, optimizer, criterion, epoch, batch_num, confusion_matrix=None):
+def train_on_batch(batch_images, batch_labels, model, optimizer, criterion, epoch, batch_num, running_loss, confusion_matrix=None):
     optimizer.zero_grad()
     if torch.cuda.is_available():
         batch_images = batch_images.cuda()
@@ -31,16 +32,23 @@ def train_on_batch(batch_images, batch_labels, model, optimizer, criterion, epoc
     loss = criterion(outputs, batch_labels)
     loss.backward()
     optimizer.step()
-    if epoch is not None:
-        print(
-            'for epoch: %s and batch: %s ' %(
-                str(epoch), str(batch_num)
-            )
+    print(
+        'for epoch: %s and batch: %s ' %(
+            str(epoch), str(batch_num)
         )
+    )
+    confusion_matrix += get_confusion_matrix(
+        model,
+        batch_images,
+        batch_labels
+    )
+    running_loss += loss.item()
     print('the loss:')
     print(loss.item())
+    return model, optimizer, running_loss, confusion_matrix
 
-def get_confusion_matrix(model, images, labels, confusion_matrix):
+def get_confusion_matrix(model, images, labels):
+    confusion_matrix = torch.zeros(2, 2)
     if torch.cuda.is_available():
         images = images.cuda()
         labels = labels.cuda()
@@ -56,18 +64,55 @@ def train_pytorch_model(model, train_data_generator, optimizer_name, optimizer_p
         optimizer_name,
     )(params = model.parameters(), **optimizer_params)
     
+    accuracies = np.zeros((epochs_num, ))
+    f_scores = np.zeros((epochs_num, ))
+    recalls = np.zeros((epochs_num, ))
+    precisions = np.zeros((epochs_num, ))
+    losses = np.zeros((epochs_num, ))
+    
     for epoch in range(epochs_num):  # loop over the dataset multiple times
         running_loss = 0.0
+        data_size = 0
+        confusion_matrix = torch.zeros(2, 2)
         for i, (batch_images, batch_labels) in enumerate(train_data_generator):
-            train_on_batch(
+            model, optimizer, running_loss, confusion_matrix = train_on_batch(
                 batch_images,
                 batch_labels,
                 model,
                 optimizer,
                 criterion,
                 epoch,
-                i
+                i,
+                running_loss,
+                confusion_matrix
             )
+            data_size += batch_images.size(0)
+        confusion_matrix = confusion_matrix.numpy()
+        accuracies[epoch] = (
+            confusion_matrix[0, 0] + confusion_matrix[1, 1]
+        ) / np.sum(confusion_matrix)
+        
+        recalls[epoch] = confusion_matrix[0, 0] / (
+            confusion_matrix[0, 0] + confusion_matrix[0, 1]
+        )
+        precisions[epoch] = confusion_matrix[0, 0] / (
+            confusion_matrix[0, 0] + confusion_matrix[1, 0]
+        )
+        f_scores[epoch] = recalls[epoch] * precisions[epoch]
+        losses[epoch] = running_loss / data_size
+    fig = plt.figure()
+    plt.subplot(3, 1, 1)
+    plt.plot(list(range(epochs_num)), f_scores)
+    plt.plot(list(range(epochs_num)), recalls)
+    plt.plot(list(range(epochs_num)), precisions)
+    plt.legend(['f_scores', 'recalls', 'precisions'])
+    plt.subplot(3, 1, 2)
+    plt.plot(list(range(epochs_num)), accuracies)
+    plt.legend(['accuracies'])
+    plt.subplot(3,1,3)
+    plt.plot(list(range(epochs_num)), losses)
+    plt.legend(['losses'])
+    
     return model
 
 def predict_with_pytorch(model_name, train_images, train_labels, test_images, test_labels, optimizer_name, optimizer_params, criterion, epochs_num, batch_size):
@@ -81,6 +126,9 @@ def predict_with_pytorch(model_name, train_images, train_labels, test_images, te
     model.features[0] = nn.Conv2d(1, 64, kernel_size=(11, 11), stride=(4, 4), padding=(2, 2))
     if torch.cuda.is_available():
         model.cuda()
+    class_weights = [
+        train_images.shape[0] / (2 * np.bincount(train_labels)[i]) for i in range(2)
+    ]
     model = train_pytorch_model(
         model,
         torch.utils.data.DataLoader(
@@ -90,7 +138,12 @@ def predict_with_pytorch(model_name, train_images, train_labels, test_images, te
             ),
             **{
                 'batch_size': batch_size,
-                'shuffle': True,
+                'sampler' : torch.utils.data.WeightedRandomSampler(
+                    [
+                        class_weights[train_label] for train_label in train_labels
+                    ],
+                    int(train_labels.shape[0])
+                ),
                 'num_workers': 6
             }
         ),
@@ -107,19 +160,22 @@ def predict_with_pytorch(model_name, train_images, train_labels, test_images, te
             data_utils.Dataset(
                 test_images,
                 test_labels
-            )
-        ),
-        confusion_matrix = torch.zeros(2, 2)
-    )
+            ),
+            batch_size = batch_size,
+            num_workers = 6
 
-def predict(model, test_data_generator, confusion_matrix):
+        )
+    )
+    plt.show()
+
+def predict(model, test_data_generator):
+    confusion_matrix = torch.zeros(2, 2)
     print('confusion matrix for testing: ')
     for batch_num, (test_images, test_labels) in enumerate(test_data_generator):
         print('for batch number: %s the confusion matrix is: ' %str(batch_num))
-        print(
-            get_confusion_matrix(model, test_images, test_labels, confusion_matrix)
-        )
-
+        confusion_matrix +=get_confusion_matrix(model, test_images, test_labels)
+        print(confusion_matrix)
+        
 def build_model(input_shape):
     #use the sequential thing
     input_img = Input(shape=input_shape, name='ImageInput')
